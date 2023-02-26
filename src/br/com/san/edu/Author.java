4b2d5ce9d591509bd9ea932b9ac40d68c960ce1b
Eduardo Santos Nascimento <edusan3456@gmail.com>
2023-02-26 13:27:07 -0300

    finish chapter 4

--- src/br/com/san/edu/Author.java
+++ src/br/com/san/edu/Author.java
@@ -1,14 +1,42 @@
 package br.com.san.edu;
 
 public class Author {
-    String name;
-    String email;
-    String cpf;
+    private String name;
+    private String email;
+    private String cpf;
+
+    public Author(String name) {
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
     void getDetails() {
         System.out.println("Mostrando detalhes do Autor!");
-        System.out.println("Nome: " + this.name);
-        System.out.println("E-mail: " + this.email);
-        System.out.println("CPF: " + this.cpf);
+        System.out.println("Nome: " + this.getName());
+        System.out.println("E-mail: " + this.getEmail());
+        System.out.println("CPF: " + this.getCpf());
     }
 }
--- src/br/com/san/edu/Book.java
+++ src/br/com/san/edu/Book.java
@@ -1,32 +1,83 @@
 package br.com.san.edu;
 
 public class Book {
-    String name;
-    String description;
-    double value;
-    String isbn;
-    Author author;
+    private String name;
+    private String description;
+    private double value;
+    private String isbn;
+    private Author author;
 
-    public Book() {
-        System.out.println("New book created!");
+    public Book(String name, double value, Author author) {
+        setName(name);
+        setValue(value);
+        setAuthor(author);
+        setIsbn("Ainda não possui um identificador");
+        System.out.println("Novo livro \"" + this.getName() + "\" criado!");
+        System.out.println("--------------------------------");
+    }
+    public Book(String name, double value, Author author, String isbn) {
+        this(name, value, author);
+        setIsbn(isbn);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
     }
 
     void getDetails() {
         System.out.println("Mostrando detalhes do livro!");
-        System.out.println("Nome: " + this.name);
-        System.out.println("Descrição: " + this.description);
-        System.out.println("Valor: R$" + this.value);
-        System.out.println("OSBN: " + this.isbn);
-        if (this.haveAuthor())
-            this.author.getDetails();
+        System.out.println("Nome: " + this.getName());
+        System.out.println("Descrição: " + this.getDescription());
+        System.out.println("Valor: R$" + this.getValue());
+        System.out.println("ISBN: " + this.getIsbn());
+        if (this.haveAuthor()) {
+            System.out.println("-----");
+            this.getAuthor().getDetails();
+        }
         System.out.println("--------------------------------");
     }
 
     void applyDiscount(double percentage) {
-        value -= value * percentage;
+        setValue(getValue() - getValue() * percentage);
     }
     boolean haveAuthor() {
-     boolean heHas = this.author != null;
-     return heHas;
+        boolean heHas = this.getAuthor() != null;
+        return heHas;
     }
 }
